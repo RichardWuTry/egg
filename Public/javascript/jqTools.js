dc52f833c72e2fbd10906265b9e0078d8a22218e
DBFocus <shadow_wu82@163.com>
2012-08-29 14:57:15 +0800

    first commit

---
+++ Public/javascript/jqTools.js
@@ -0,0 +1,29 @@
+function slideBox(msgBox, duration, fColor, bgColor) {
+	var box = $(msgBox);
+	if (typeof bgColor !== 'undefined') {
+		box.css('background-color', bgColor);
+	}
+	if (typeof fColor !== 'undefined') {
+		box.css('color', fColor);
+	}
+	duration = typeof duration !== 'undefined' ? duration : 3000;
+	box.slideDown(function() {
+					setTimeout(function() { box.slideUp(); }, 
+					duration)});
+}
+
+function showBox(msgBox, fColor, bgColor) {
+	var box = $(msgBox);
+	if (typeof bgColor !== 'undefined') {
+		box.css('background-color', bgColor);
+	}
+	if (typeof fColor !== 'undefined') {
+		box.css('color', fColor);
+	}
+	box.show();
+}
+
+function nl2br(str, is_xhtml) {   
+	var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '\<br \/>' : '\<br>';    
+	return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1'+ breakTag +'$2');
+}
